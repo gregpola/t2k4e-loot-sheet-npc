0a7e4a5e9a62c88c779b4ed974b342c70f263a2a
gpola <greg.w.pola@gmail.com>
2022-08-02 13:05:32 -0700

    Trying to fix gear actions

--- scripts/helper/HandlebarsHelper.js
+++ scripts/helper/HandlebarsHelper.js
@@ -19,6 +19,8 @@ export class HandlebarsHelper {
      */
     static register() {
 
+        Handlebars.registerHelper('isGM', () => game.user.isGM);
+
         /**
          * If a equals b
          *
